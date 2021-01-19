aa25d13e1442709015056884970d69be226a40c8
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2021-01-19 21:39:43 +0700

    fitur hot reload

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter App Demo Home Page'),
     );
   }
 }
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'You have clicked the button this many times',
             ),
             Text(
               '$_counter',
