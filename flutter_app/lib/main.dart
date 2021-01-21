840337475aaa2556ef36b721782bab20da7cea6f
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2021-01-21 19:14:12 +0700

    StatelessWidget

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -13,8 +13,27 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: Center(
-          child: Text("Hello world!"),
+          child: Heading(
+              text:"Hello world!",
+              ),
         ),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  Heading({Key key, this.text});
+
+  @override
+  Widget build(BuildContext context){
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
       ),
     );
   }
