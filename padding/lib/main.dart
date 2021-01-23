5ec20c8f4a84297fb3512db0f68e3dbb824f10d6
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2021-01-23 21:14:29 +0700

    padding

---
+++ padding/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstScreen(),// Panggil FirstScreen di sini
+
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white,),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(100),
+        child: Text(
+          'Ini Padding',
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
