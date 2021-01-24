f0900dffac0bec19fcae8c40737b8268cf49f515
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2021-01-24 23:21:30 +0700

    row

---
+++ row_coloum/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstScreen(),// Panggil FirstScreen di sini
+
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white,),
+          )
+        ],
+      ),
+      body: Row(        //Membuat Row
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget> [
+          IconButton(icon: Icon(Icons.share)),
+          IconButton(icon: Icon(Icons.thumb_up)),
+          IconButton(icon: Icon(Icons.thumb_down)),
+        ],
+      )
+    );
+  }
+}
